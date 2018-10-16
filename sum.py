195c1cd2a84c1f90c2531793cd5e6df2c2bc00d4
Eren Sulutas <erensulu11@gmail.com>
2018-10-16 17:06:03 -0400

    Update sum.py
    final fix

--- sum.py
+++ sum.py
@@ -1,10 +1,13 @@
 div = 0
+sum = 0
 
 startnum = int(input("Start at: "))
 endnum = int(input("End at: "))
 divisor = int(input("Divisor: "))
 
-for i in range(startnum, (endnum + 1), divisor):
-	div += 1
+for i in range(startnum, (endnum + 1)):
+	if i % divisor == 0:
+		sum += i
+		div += 1
 	
-print(div)
+print("Sum: {}\n# of integers added: {}".format(sum, div))
